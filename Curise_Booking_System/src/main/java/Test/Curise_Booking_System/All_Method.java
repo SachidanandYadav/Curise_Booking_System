e85f23f2333caf96a7e7e1680ed74cb2b820c02f
v2stech <v2stech@v2stech-Latitude-3520>
2022-03-07 16:57:12 +0530

    second Commit

--- Curise_Booking_System/src/main/java/Test/Curise_Booking_System/All_Method.java
+++ Curise_Booking_System/src/main/java/Test/Curise_Booking_System/All_Method.java
@@ -9,124 +9,179 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
-
-
 
 public class All_Method {
 	Scanner sc = new Scanner(System.in);
-	List<Data> al3 = new ArrayList<Data>();
+
 	boolean flag = false;
-	public List<Data> read_file() throws IOException, CsvValidationException {
+
+	public List<Data> read_file(String name1) {
 		List<Data> al = new ArrayList<Data>();
-		int count = 0;
-		//String path = ("src/main/resources/"+name+".csv");
-		String path = ("src/main/resources/Customer_Data.csv");
-		CSVReader reader = new CSVReader(new FileReader(path));
-		String[] nextLine;
+		try {
+			int count = 0;
+			String path = ("src/main/resources/" + name1 + ".csv");
+			// String path = ("src/main/resources/Customer_Data.csv");
+			CSVReader reader = new CSVReader(new FileReader(path));
+			String[] nextLine;
 			while ((nextLine = reader.readNext()) != null) {
+				if((nextLine[2].endsWith(".com") || nextLine[2].endsWith(".in")) && nextLine[2].contains("@")) {
 				Data user = new Data();
-				
 				user.setId(nextLine[0]);
 				user.setName(nextLine[1]);
 				user.setEmail(nextLine[2]);
 				al.add(user);
 				count++;
-				
+				}
 			}
-			System.out.println("Number of record read : "+count);
+			System.out.println("Number of record read : " + count);
+		} catch (Exception e) {
+			System.out.println(name1 + " Not Found . Enter Valid name.");
+		}
 		return al;
 	}
-	
-	public void display(List<Data> al) {
-		System.out.println("All Customer Data.");
-		System.out.println("Cust_Id\tCust_Name");
-		for (Data user : al) 
-				System.out.println(user.getId()+"\t"+user.getName());
-		}	
 
-	public void search(String cust_name, List<Data> al) {
+	public void display(List<Data> al) {
+
+		System.out.println("All Customer Data.");
+		System.out.println("Cust_Id\tCust_Name\tCust_Email");
+		for (Data user : al)
+			System.out.println(user.getId() + "\t" + user.getName()+ "\t" + user.getEmail());
+	}
+
+	public void searchByname(String cust_name, List<Data> al) {
+		int count = 0;
 		System.out.println("Cust_Id\tCust_Name");
 		for (Data user : al) {
-			if (user.getName().equals(cust_name))
-				System.out.println(user.getId()+"\t"+user.getName());
-			flag = true;
-		}	
-		if(flag = false)
-			System.out.println(cust_name+" Not found.");
+			if (user.getName().equals(cust_name)) {
+				System.out.println(user.getId() + "\t" + user.getName());
+				count++;
+			}
+		}
+		if (count == 0)
+			System.out.println(cust_name + " Not found.Enter Valid name.");
+	}
 
+	public void searchByid(String cust_id, List<Data> al) {
+		int count = 0;
+		System.out.println("Cust_Id\tCust_Name");
+		for (Data user : al) {
+			if (user.getId().equals(cust_id)) {
+				System.out.println(user.getId() + "\t" + user.getName());
+				count++;
+			}
+		}
+		if (count == 0)
+			System.out.println(cust_id + " Not found.Enter Valid name.");
 	}
 
 	public void booking(String name, List<Data> al) {
+		All_Method obj = new All_Method();
+		int count = 0;
 		for (Data user : al) {
 			if (user.getName().equals(name)) {
-				System.out.println("Enter the day :");
-				int day = sc.nextInt();
-				System.out.println("Enter the month :");
-				int month = sc.nextInt();
-				System.out.println("Enter the year :");
-				int year = sc.nextInt();
-				String date = (day+"/"+month+"/"+year);
-				System.out.println("Date is "+date);
+				String date = obj.date_valid();
+				System.out.println("Date is " + date);
 				user.setDate(date);
-				
-				System.out.println("Booking of "+name+" Confirmed. "+user.getDate());
-				flag = true;
+
+				System.out.println("Booking of " + name + " Confirmed. " + user.getDate());
+				count++;
 			}
 		}
-		if(flag = false)
-			System.out.println(name+" Not found.");
+		if (count == 0)
+			System.out.println(name + " Not found.");
 	}
 
-	public void booking_date(String b_date, List<Data> al) {
+	public void booking_date(String date, List<Data> al) {
+		int count1 = 0;
 		System.out.println("Cust_Id\tCust_Name");
 		int count = 0;
 		for (Data user : al) {
 			try {
-				if (user.getDate().equals(b_date))
+				if (user.getDate().equals(date)) {
 					System.out.println(user.getId() + "\t" + user.getName());
-				count++;
-			} catch (Exception e) {
-				
-			}
-			flag =true;
+					count++;
+					count1++;
+				}
+			} catch (Exception e) { }
 		}
-	System.out.println("Number of attendees : "+ count);
-	if(flag = false)
-		System.out.println(b_date+" Not found.");
+		if (count == 0) 
+			System.out.println(date + " Not found.");
+			
+		System.out.println("Number of attendees : " + count);
 	}
-	
 
-	public void save_data(int day,int month,int year, List<Data> al) throws IOException {
-		
-		String date2 = (day+"/"+month+"/"+year);
+	public void save_data(int day, int month, int year, List<Data> al) throws IOException {
+
+		String date2 = (day + "/" + month + "/" + year);
+		List<Data> al3 = new ArrayList<Data>();
 		for (Data user : al) {
 			try {
 				if (user.getDate().equals(date2))
 					al3.add(user);
-				System.out.println("Data Add Succesfully");
-			} catch (Exception e) {
-				
+			} catch (Exception e) { }
+		}
+		try {
+			String path = ("src/main/resources/" + day + "-" + month + "-" + year + ".csv");
+			File file = new File(path);
+			file.createNewFile();
+			FileWriter writer = new FileWriter(path);
+			writer.append("Cust_ID ,Cust_Name,Booking Data,\n");
+			for (Data list : al3) {
+				writer.append(list.getId());
+				writer.append(",");
+				writer.append(list.getName());
+				writer.append(",");
+				writer.append(list.getDate() + "\n");
+			}
+			writer.close();
+		} catch (Exception e) {
+			System.out.println("Something Wrong.");
+		}
+		System.out.println("Data Stored in Excle File.");
+	}
+	
+	
+	public String date_valid() {
+		System.out.println("Enter Year in YYYY Formate");
+		int year = sc.nextInt();
+		while (year < 1000 || year > 2022) {
+			System.out.println("Enter Valid year");
+			year = sc.nextInt();
+		}
+		System.out.println("Enter Month in MM Formate");
+		int month = sc.nextInt();
+		while (month > 12 || month <= 0) {
+			System.out.println("Enter Valid month");
+			month = sc.nextInt();
+		}
+		System.out.println("Enter Day in DD Formate");
+		int day = sc.nextInt();
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			while (day <= 0 || day > 31) {
+				System.out.println("Enter Valid Day");
+				day = sc.nextInt();
+			}
+		} else if (month == 2) {
+			if (year % 4 == 0) {
+				while (day <= 0 || day > 29) {
+					System.out.println("Enter Valid Day");
+					day = sc.nextInt();
+				}
+			} else {
+				while (day <= 0 || day > 28) {
+					System.out.println("Enter Valid Day");
+					day = sc.nextInt();
+				}
+			}
+
+		} else {
+			while (day <= 0 || day > 30) {
+				System.out.println("Enter Valid Day");
+				day = sc.nextInt();
 			}
 		}
-		
-		try {
-			String path = ("src/main/resources/"+day+"-"+month+"-"+year+".csv");
-					File file = new File(path);
-					file.createNewFile();
-					FileWriter writer = new FileWriter(path);
-					writer.append("Cust_ID ,Cust_Name,Booking Data,\n");
-					for (Data list : al3) {
-						writer.append(list.getId());
-						writer.append(",");
-						writer.append(list.getName());
-						writer.append(",");
-						writer.append(list.getDate() + "\n");
-					}
-					writer.close();
-					}catch (Exception e) {
-						// TODO: handle exception
-					}
-	}
 
+		String date = (day + "/" + month + "/" + year);
+		return date;
+	}
 }
--- Curise_Booking_System/src/main/java/Test/Curise_Booking_System/App.java
+++ Curise_Booking_System/src/main/java/Test/Curise_Booking_System/App.java
@@ -8,16 +8,14 @@ import java.util.Scanner;
 
 import com.opencsv.exceptions.CsvValidationException;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
 	public static void main(String[] args) throws IOException, CsvValidationException {
 		Scanner sc = new Scanner(System.in);
 		List<Data> al = new ArrayList<Data>();
 		All_Method obj = new All_Method();
-		int choice, search_ac;
+		int choice;
+		boolean flag = false;
 		do {
 			System.out.println("============ MAIN MENU =============");
 			System.out.println("\n* Select any one Option *");
@@ -30,45 +28,108 @@ public class App {
 
 			switch (choice) {
 			case 1:		
-//				System.out.println("Enter the name of the data file :");
-//				String name = sc.next();
-					List<Data> data = obj.read_file();
+				System.out.println("Enter the name of the data file :");
+				String name1 = sc.next();
+					List<Data> data = obj.read_file(name1);
 					al.addAll(data);
+					flag = true;
 				break;
 			case 2:
+				if(flag == true)
 				obj.display(al);
+				else
+					System.out.println("First Read the customer data .");
 				break;
 			case 3:
-				System.out.println("Enter the customer name");
-				String cust_name = sc.next();
-				obj.search(cust_name,al);
+				if(flag == true) {
+					System.out.println("[1].Search by ID\n[2].Search by Name.");
+					int ch = sc.nextInt();
+					if(ch == 1) {
+						System.out.println("Enter the customer id");
+						String cust_id = sc.next();
+						obj.searchByid(cust_id,al);
+					}else if(ch == 2){
+						System.out.println("Enter the customer name");
+						String cust_name = sc.next();
+						obj.searchByname(cust_name,al);
+					}else {
+						System.out.println("Enter valid option.");
+					}
+				
+				}else {
+					System.out.println("First Read the customer data .");
+				}
 				break;
 			case 4:
+				if(flag == true) {
 				System.out.println("Enter the name :");
 				String name = sc.next();
 				obj.booking(name,al);
+				}else {
+					System.out.println("First Read the customer data .");
+				}
 				break;
 			case 5:
-				System.out.println("Enter the date :");
-				String b_date = sc.next();
-				obj.booking_date(b_date,al);
+				if(flag == true) {
+				String date = obj.date_valid();	
+				obj.booking_date(date,al);
+				}else {
+					System.out.println("First Read the customer data .");
+				}
 				break;
 			case 6:
-				System.out.println("Enter the day :");
-				int day = sc.nextInt();
-				System.out.println("Enter the month :");
-				int month = sc.nextInt();
-				System.out.println("Enter the data :");
-				int year = sc.nextInt();
+				if(flag == true) {
+					System.out.println("Enter Year in YYYY Formate");
+					int year = sc.nextInt();
+					while (year < 1000 || year > 2022) {
+						System.out.println("Enter Valid year");
+						year = sc.nextInt();
+					}
+					System.out.println("Enter Month in MM Formate");
+					int month = sc.nextInt();
+					while (month > 12 || month <= 0) {
+						System.out.println("Enter Valid month");
+						month = sc.nextInt();
+					}
+					System.out.println("Enter Day in DD Formate");
+					int day = sc.nextInt();
+					if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+						while (day <= 0 || day > 31) {
+							System.out.println("Enter Valid Day");
+							day = sc.nextInt();
+						}
+					} else if (month == 2) {
+						if (year % 4 == 0) {
+							while (day <= 0 || day > 29) {
+								System.out.println("Enter Valid Day");
+								day = sc.nextInt();
+							}
+						} else {
+							while (day <= 0 || day > 28) {
+								System.out.println("Enter Valid Day");
+								day = sc.nextInt();
+							}
+						}
+
+					} else {
+						while (day <= 0 || day > 30) {
+							System.out.println("Enter Valid Day");
+							day = sc.nextInt();
+						}
+					}
 				obj.save_data(day,month,year,al);
+				}else {
+					System.out.println("First Read the customer data .");
+				}
 			break;
 			case 0:
 				System.exit(0);
 				break;
 			default:
 				System.out.println("Enter valid number.");
+				break;
 			}
-		} while (choice < 7);
+		} while (choice != 0);
 
 	}
 }
